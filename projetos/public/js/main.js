3c3cb1c8c1194e677e0ccd3f598649464978bb2d
Rodrigo Martins <contawarface750@gmail.com>
2018-04-28 16:20:58 -0300

    Update main.js

--- projetos/public/js/main.js
+++ projetos/public/js/main.js
@@ -35,7 +35,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     RESET.addEventListener('click', function() {
-        console.log('click');
         reset();
         alguemVenceu = false;
         jogadas = 0;
