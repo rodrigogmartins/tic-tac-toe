7bf43b607151777ffee30eec7c88775b929531bd
Rodrigo Martins <rodrigo.martins@ubots.com.br>
2022-07-24 22:18:35 -0300

    Formatação

--- public/js/GameService.js
+++ public/js/GameService.js
@@ -1,48 +1,48 @@
 export class GameService {
-    realizaJogada (celula, simbolo) {
-        celula.setAttribute('disabled', true)
+  realizaJogada (celula, simbolo) {
+    celula.setAttribute('disabled', true)
 
-        return (celula.textContent = simbolo)
+    return (celula.textContent = simbolo)
+  }
+
+  verificaVitoria (celulas, simbolo) {
+    return (
+      this.vitoriaVertical(celulas, simbolo) ||
+      this.vitoriaHorizontal(celulas, simbolo) ||
+      this.vitoriaDiagonal(celulas, simbolo)
+    )
+  }
+
+  vitoriaVertical (celulas, simbolo) {
+    return (
+      this.vitoria(celulas, 0, 3, simbolo) ||
+      this.vitoria(celulas, 1, 3, simbolo) ||
+      this.vitoria(celulas, 2, 3, simbolo)
+    )
+  }
+
+  vitoriaHorizontal (celulas, simbolo) {
+    return (
+      this.vitoria(celulas, 0, 1, simbolo) ||
+      this.vitoria(celulas, 3, 1, simbolo) ||
+      this.vitoria(celulas, 6, 1, simbolo)
+    )
+  }
+
+  vitoriaDiagonal (celulas, simbolo) {
+    return (
+      this.vitoria(celulas, 0, 4, simbolo) ||
+      this.vitoria(celulas, 2, 2, simbolo)
+    )
+  }
+
+  vitoria (celulas, indiceInicial, incremento, simbolo) {
+    let result = true
+
+    for (let i = indiceInicial, j = 0; j < 3; i += incremento, j++) {
+      result = result && celulas[i].textContent === simbolo
     }
 
-    verificaVitoria (celulas, simbolo) {
-        return (
-            this.vitoriaVertical(celulas, simbolo) ||
-            this.vitoriaHorizontal(celulas, simbolo) ||
-            this.vitoriaDiagonal(celulas, simbolo)
-        )
-    }
-
-    vitoriaVertical (celulas, simbolo) {
-        return (
-            this.vitoria(celulas, 0, 3, simbolo) ||
-            this.vitoria(celulas, 1, 3, simbolo) ||
-            this.vitoria(celulas, 2, 3, simbolo)
-        )
-    }
-
-    vitoriaHorizontal (celulas, simbolo) {
-        return (
-            this.vitoria(celulas, 0, 1, simbolo) ||
-            this.vitoria(celulas, 3, 1, simbolo) ||
-            this.vitoria(celulas, 6, 1, simbolo)
-        )
-    }
-
-    vitoriaDiagonal (celulas, simbolo) {
-        return (
-            this.vitoria(celulas, 0, 4, simbolo) ||
-            this.vitoria(celulas, 2, 2, simbolo)
-        )
-    }
-
-    vitoria (celulas, indiceInicial, incremento, simbolo) {
-        let result = true
-
-        for (let i = indiceInicial, j = 0; j < 3; i += incremento, j++) {
-            result = result && celulas[i].textContent === simbolo
-        }
-
-        return result
-    }
+    return result
+  }
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,81 +1,79 @@
 import { GameService } from './GameService.js'
 
 document.addEventListener('DOMContentLoaded', function () {
-    const GAME = new GameService()
-    const TABULEIRO = document.querySelector('#tabuleiro')
-    const NOVO_JOGO_BTN = document.querySelector('#novo')
-    const RESET_BTN = document.querySelector('#reset')
-    const MAXJOGADAS = 9
-    let jogadas = 0
-    let jogoAcabou = false
+  const GAME = new GameService()
+  const TABULEIRO = document.querySelector('#tabuleiro')
+  const NOVO_JOGO_BTN = document.querySelector('#novo')
+  const RESET_BTN = document.querySelector('#reset')
+  const MAXJOGADAS = 9
+  let jogadas = 0
+  let jogoAcabou = false
 
-    TABULEIRO.addEventListener('click', function (e) {
-        if (
-            e.target.className === 'celula' &&
-            e.target.getAttribute('disabled') !== 'true' &&
-            !jogoAcabou &&
-            jogadas < MAXJOGADAS
-        ) {
-            const celula = e.target
-            const celulas = document.querySelectorAll('.celula')
-            const simbolo = jogadas % 2 === 0 ? 'X' : 'O'
+  TABULEIRO.addEventListener('click', function (e) {
+    if (
+      e.target.className === 'celula' &&
+      e.target.getAttribute('disabled') !== 'true' &&
+      !jogoAcabou &&
+      jogadas < MAXJOGADAS
+    ) {
+      const celula = e.target
+      const celulas = document.querySelectorAll('.celula')
+      const simbolo = jogadas % 2 === 0 ? 'X' : 'O'
 
-            GAME.realizaJogada(celula, simbolo)
-            jogadas++
+      GAME.realizaJogada(celula, simbolo)
+      jogadas++
 
-            if (GAME.verificaVitoria(celulas, simbolo)) {
-                jogoAcabou = true
-                desabilitarCelulas()
-                contabilizaResultado(simbolo)
-            } else if (jogadas === MAXJOGADAS) {
-                jogoAcabou = true
-                desabilitarCelulas()
-                contabilizaResultado('EMPATE')
-            }
-        }
-    })
-
-    NOVO_JOGO_BTN.addEventListener('click', reset)
-
-    RESET_BTN.addEventListener('click', function () {
-        reset()
-        resetResultado()
-    })
-
-    function contabilizaResultado (simbolo) {
-        const seletores = {
-            X: '#vitoriasJogador1',
-            O: '#vitoriasJogador2',
-            EMPATE: '#empates'
-        }
-
-        console.log(seletores[simbolo])
-        const contador = document.querySelector(seletores[simbolo])
-        contador.value++
+      if (GAME.verificaVitoria(celulas, simbolo)) {
+        jogoAcabou = true
+        desabilitarCelulas()
+        contabilizaResultado(simbolo)
+      } else if (jogadas === MAXJOGADAS) {
+        jogoAcabou = true
+        desabilitarCelulas()
+        contabilizaResultado('EMPATE')
+      }
     }
+  })
 
-    function resetResultado () {
-        document.querySelector('#vitoriasJogador1').value = 0
-        document.querySelector('#vitoriasJogador2').value = 0
-        document.querySelector('#empates').value = 0
+  NOVO_JOGO_BTN.addEventListener('click', reset)
+
+  RESET_BTN.addEventListener('click', function () {
+    reset()
+    resetResultado()
+  })
+
+  function contabilizaResultado (simbolo) {
+    const seletores = {
+      X: '#vitoriasJogador1',
+      O: '#vitoriasJogador2',
+      EMPATE: '#empates'
     }
+    const contador = document.querySelector(seletores[simbolo])
+    contador.value++
+  }
 
-    function desabilitarCelulas () {
-        const CELULAS = document.querySelectorAll('.celula')
+  function resetResultado () {
+    document.querySelector('#vitoriasJogador1').value = 0
+    document.querySelector('#vitoriasJogador2').value = 0
+    document.querySelector('#empates').value = 0
+  }
 
-        for (const BOTAO of CELULAS) {
-            BOTAO.setAttribute('disabled', true)
-        }
+  function desabilitarCelulas () {
+    const CELULAS = document.querySelectorAll('.celula')
+
+    for (const BOTAO of CELULAS) {
+      BOTAO.setAttribute('disabled', true)
     }
+  }
 
-    function reset () {
-        jogadas = 0
-        jogoAcabou = false
-        const CELULAS = document.querySelectorAll('.celula')
+  function reset () {
+    jogadas = 0
+    jogoAcabou = false
+    const CELULAS = document.querySelectorAll('.celula')
 
-        for (const BOTAO of CELULAS) {
-            BOTAO.removeAttribute('disabled')
-            BOTAO.innerHTML = '&nbsp;&nbsp;&nbsp;'
-        }
+    for (const BOTAO of CELULAS) {
+      BOTAO.removeAttribute('disabled')
+      BOTAO.innerHTML = '&nbsp;&nbsp;&nbsp;'
     }
+  }
 })
